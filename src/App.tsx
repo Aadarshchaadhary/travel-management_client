63e905fbda1632843e8b76337f123c8911201621
Aadarsh <adarshry869@gmail.com>
2025-09-19 19:20:23 +0545

    setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <h1 className="text-blue-950 text-9xl font-extrabold">Hello Sir G</h1>
+      <h2>This is my tour management system</h2>
+    </main>
+  );
+}
+
+export default App;
