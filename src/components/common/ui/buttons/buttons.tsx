4bc8cf76ccfbf1efd77a24746c29131fbaafc1d0
AADARSH CHAUDHARY <aadarshchaudhary@AADARSHs-MacBook-Air.local>
2025-10-20 18:58:05 +0545

    .

--- src/components/common/ui/buttons/buttons.tsx
+++ src/components/common/ui/buttons/buttons.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { LiaSignInAltSolid } from "react-icons/lia";
+import { GiClick } from "react-icons/gi";
 
 interface IProps {
   label: string;
@@ -7,16 +7,19 @@ interface IProps {
   icon?: React.ReactNode;
 }
 
-const Button: React.FC<IProps> = ({ label, type = "button" }) => {
+const Button: React.FC<IProps> = ({
+  label,
+  type = "button",
+  icon = <GiClick size={26} />,
+}) => {
   return (
     <div className="w-full">
       <button
         type={type}
-        className="cursor-pointer text center text-white 
-        font-bold tex-lg transition all duration-300 bg-blue-700 hover:bg-blue-700 w-full mt-10 py-3.5"
-        rounded-md
+        className="cursor-pointer text-center text-white font-bold text-lg transition-all
+               duration-300 bg-blue-600 hover:bg-blue-700 w-full mt-10 py-3.5 rounded-md flex items-center justify-center gap-2"
       >
-        <div>LiaSignInAltSolid size={26}</div>
+        {icon && <div>{icon}</div>}
         <span> {label}</span>
       </button>
     </div>
